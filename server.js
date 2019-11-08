f881d2bb0d9ef403c4df5d66a69217dea5745680
NDIFE <stilesndife@gmail.com>
2019-11-08 10:18:53 +0100

    change port config

--- server.js
+++ server.js
@@ -129,7 +129,7 @@ if(process.env.NODE_ENV === 'production'){
 }
 
 
+const port = process.env.PORT || 5000;
 
-app.listen(4000, () => {
-    console.log('products server listening on port 4000')
-})
+app.listen(port, () => console.log(`Server started on port ${port}`))
+
